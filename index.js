68a65a23563059a9c81445e3f6343b249d25bf63
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-02-11 16:24:56 +0100

    chore: cleanup

--- index.js
+++ index.js
@@ -10,42 +10,15 @@ module.exports = {
     browser: true,
   },
   rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'comma-dangle': ['error', 'always-multiline'],
     'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'comma-dangle': ['error', 'always-multiline'],
     'import/prefer-default-export': 'off',
     // Depricated (remove when it's gone from airbnb's config)
     'jsx-a11y/label-has-for': 'off',
-    // Maybe enable once it's less buggy
+    'react-hooks/rules-of-hooks': 'error',
     'react/destructuring-assignment': 'off',
     'react/jsx-filename-extension': 'off',
     'react/prop-types': 'off',
-    // Maybe enable once it works better with class fields
-    'react/sort-comp': [
-      'off',
-      {
-        order: [
-          'static-methods',
-          'constructor',
-          'instance-variables',
-          'lifecycle',
-          'everything-else',
-          'rendering',
-        ],
-        groups: {
-          lifecycle: [
-            'state',
-            'componentWillMount',
-            'componentDidMount',
-            'componentWillReceiveProps',
-            'shouldComponentUpdate',
-            'componentWillUpdate',
-            'componentDidUpdate',
-            'componentWillUnmount',
-          ],
-          rendering: ['/^render.+$/', 'render'],
-        },
-      },
-    ],
+    'react/sort-comp': 'off',
   },
 }

402a2d94cf6c2417946b0d2d84a24103526aebca
Alexander Nanberg <alex.nanberg@hotmail.com>
2022-02-25 14:09:15 +0100

    feat(typescript): enable  rule

--- typescript.js
+++ typescript.js
@@ -15,19 +15,26 @@ module.exports = {
       },
       extends: [
         'plugin:import/typescript',
+        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'prettier',
       ],
       rules: {
         //https://github.com/typescript-eslint/typescript-eslint/issues/2483
         'no-shadow': 'off',
+        // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-shadow.md
         '@typescript-eslint/no-shadow': 'error',
+        // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/consistent-type-imports.md
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { prefer: 'type-imports' },
+        ],
       },
     },
     {
       files: ['*.test.ts', '*.test.tsx'],
       rules: {
-        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
+        // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
         '@typescript-eslint/explicit-function-return-type': 0,
       },
     },

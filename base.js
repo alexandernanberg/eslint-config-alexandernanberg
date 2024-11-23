c8163cc47aa0ab3f97e4e4ed87b86fe85318c1e8
Alexander Nanberg <alex.nanberg@hotmail.com>
2024-11-23 09:16:26 +0100

    feat: upgrade to eslint v9
    BREAKING CHANGE: eslint v9 is now required

--- base.js
+++ base.js
@@ -1,98 +1,128 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:prettier/recommended',
-  ],
-  env: {
-    browser: true,
-    node: true,
+// @ts-check
+/// <reference types="./types.d.ts" />
+
+import eslint from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,ts,tsx}'],
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      // require let or const instead of var
+      // https://eslint.org/docs/rules/no-var
+      'no-var': 'error',
+
+      // require method and property shorthand syntax for object literals
+      // https://eslint.org/docs/rules/object-shorthand
+      'object-shorthand': [
+        'error',
+        'always',
+        {
+          ignoreConstructors: false,
+          avoidQuotes: true,
+        },
+      ],
+
+      // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/__test__/**',
+            '**/__tests__/**',
+            '**/test.{js,ts,tsx}',
+            '**/*.test.{js,ts,tsx}',
+            '**/.storybook/**',
+            '**/stories/**',
+            '**/*.stories.{js,ts,tsx}',
+            '**/*.story.{js,ts,tsx}',
+            '**/jest.setup.{js,ts}',
+            '**/setupTests.js',
+            '**/webpack.*.{js,ts}',
+            '**/rollup.*.{js,ts}',
+            '**/babel.*.js',
+            '**/*.config.*',
+          ],
+        },
+      ],
+
+      // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/namespace.md
+      'import/namespace': ['error', { allowComputed: true }],
+
+      // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+      'import/order': [
+        'error',
+        { groups: [['builtin', 'external', 'internal']] },
+      ],
+
+      // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-cycle.md
+      'import/no-cycle': 'error',
+
+      // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-useless-path-segments.md
+      'import/no-useless-path-segments': 'error',
+
+      // https://github.com//blob/main/docs/rules/consistent-type-specifier-style.md
+      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    },
   },
-  rules: {
-    // disallow expressions where the operation doesn't affect the value
-    // https://eslint.org/docs/rules/no-constant-binary-expression
-    'no-constant-binary-expression': 'error',
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
+    rules: {
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/consistent-type-imports.mdx
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+      ],
 
-    // require let or const instead of var
-    // https://eslint.org/docs/rules/no-var
-    'no-var': 'error',
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-empty-object-type.mdx
+      '@typescript-eslint/no-empty-object-type': [
+        'error',
+        { allowInterfaces: 'always' },
+      ],
 
-    // disallow declaration of variables already declared in the outer scope
-    // https://eslint.org/docs/rules/no-shadow
-    'no-shadow': 'error',
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-empty-interface.mdx
+      '@typescript-eslint/no-empty-interface': 'off',
 
-    // disallow declaration of variables that are not used in the code
-    // https://eslint.org/docs/rules/no-unused-vars
-    'no-unused-vars': [
-      'error',
-      { vars: 'all', args: 'after-used', ignoreRestSiblings: true },
-    ],
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-misused-promises.mdx
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
 
-    // require method and property shorthand syntax for object literals
-    // https://eslint.org/docs/rules/object-shorthand
-    'object-shorthand': [
-      'error',
-      'always',
-      {
-        ignoreConstructors: false,
-        avoidQuotes: true,
-      },
-    ],
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-shadow.mdx
+      '@typescript-eslint/no-shadow': 'error',
 
-    // suggest using of const declaration for variables that are never modified after declared
-    // https://eslint.org/docs/rules/prefer-const
-    'prefer-const': [
-      'error',
-      {
-        destructuring: 'all',
-        ignoreReadBeforeAssign: true,
-      },
-    ],
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-unnecessary-condition.mdx
+      '@typescript-eslint/no-unnecessary-condition': 'error',
 
-    // https://github.com/prettier/eslint-plugin-prettier
-    'prettier/prettier': 'warn',
-
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: [
-          '**/test/**',
-          '**/tests/**',
-          '**/test.{js,ts,tsx}',
-          '**/*.test.{js,ts,tsx}',
-          '**/spec/**',
-          '**/specs/**',
-          '**/spec.{js,ts,tsx}',
-          '**/*.spec.{js,ts,tsx}',
-          '**/cypress/**',
-          '**/.storybook/**',
-          '**/stories/**',
-          '**/*.stories.{js,ts,tsx}',
-          '**/*.story.{js,ts,tsx}',
-          '**/jest.setup.{js,ts}',
-          '**/setupTests.js',
-          '**/webpack.*.{js,ts}',
-          '**/rollup.*.{js,ts}',
-          '**/babel.*.js',
-        ],
-      },
-    ],
-
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/namespace.md
-    'import/namespace': ['error', { allowComputed: true }],
-
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
-    'import/order': [
-      'error',
-      { groups: [['builtin', 'external', 'internal']] },
-    ],
-
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-cycle.md
-    'import/no-cycle': 'error',
-
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-useless-path-segments.md
-    'import/no-useless-path-segments': 'error',
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-unused-vars.mdx
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
   },
-}
+  {
+    linterOptions: { reportUnusedDisableDirectives: 'warn' },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+    },
+  },
+)
--- react.js
+++ react.js
@@ -1,31 +1,25 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: [
-    'plugin:import/react',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'prettier',
-  ],
-  settings: {
-    react: {
-      version: 'detect',
+import reactPlugin from 'eslint-plugin-react'
+import hooksPlugin from 'eslint-plugin-react-hooks'
+import reactCompilerPlugin from 'eslint-plugin-react-compiler'
+
+/** @type {Awaited<import('typescript-eslint').Config>} */
+export default [
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': hooksPlugin,
+      'react-compiler': reactCompilerPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs['jsx-runtime'].rules,
+      ...hooksPlugin.configs.recommended.rules,
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
+      'react/self-closing-comp': 'error',
+      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
+      'react/jsx-boolean-value': ['error', 'never'],
+      // https://github.com/facebook/react/tree/main/compiler/packages/eslint-plugin-react-compiler
+      'react-compiler/react-compiler': 'error',
     },
   },
-  plugins: ['eslint-plugin-react-compiler'],
-  rules: {
-    // https://github.com/facebook/react/tree/main/compiler/packages/eslint-plugin-react-compiler
-    'react-compiler/react-compiler': 'error',
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
-    'react/display-name': ['off', { ignoreTranspilerName: false }],
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/button-has-type.md
-    'react/button-has-type': 'error',
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
-    'react/self-closing-comp': 'error',
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
-    'react/jsx-boolean-value': ['error', 'never'],
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
-    'react/prop-types': 'off',
-  },
-}
+]
